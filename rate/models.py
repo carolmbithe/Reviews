4bd95ca912026989971c8a2a180085f79ea856c9
carolmbithe <carombithe97@gmail.com>
2018-11-16 12:01:11 +0300

    initial migration

--- rate/models.py
+++ rate/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+# from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo=models.ImageField(upload_to='profiles',blank=True)
+    # user = models.OneToOneField(User,null = True,on_delete=models.CASCADE,related_name = "profile")
+    bio=models.TextField(blank=True,null=True)
+    contact=models.EmailField(blank=True,null=True)
+
+    def __str__(self):
+        return self.user
+
+    def save_profile(self):
+        self.save()
+    def delete_profile(self):
+        self.delete()
+
+
+
+class Project(models.Model):
+    title=models.CharField(max_length =30)
+    image=models.ImageField(upload_to ='projects/', blank = True)
+    description=models.TextField(blank=True,null=True)
+    # profile=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    # link=models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+    def delete_project(self):
+        self.delete()
