1b37cb101ed6e68ddd5f9553468261c32e24a49b
carolmbithe <carombithe97@gmail.com>
2018-11-16 11:10:55 +0300

    bootstrap and static files

--- Review/settings.py
+++ Review/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    
+
     'bootstrap3',
     'rate.apps.RateConfig',
     'django.contrib.admin',
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
