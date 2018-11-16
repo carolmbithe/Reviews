aac771678fe1516720eac8502d17b8c2b51a54f6
carolmbithe <carombithe97@gmail.com>
2018-11-16 15:47:16 +0300

    Created a user

--- rate/models.py
+++ rate/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import URLValidator
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Profile(models.Model):
